d99d7cf5c85152fb1b8b9a1b6b11467f0ec05774
galslogas-droid <galslogas@gmail.com>
2026-09-11 22:43:31 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+// Service Worker Cache Offline untuk Kopontren Gus Lim
+const CACHE_NAME = 'kopontren-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './catatan_kopontren_guslim.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((k) => {
+          if (k !== CACHE_NAME) return caches.delete(k);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request).catch(() => caches.match('./catatan_kopontren_guslim.html'));
+    })
+  );
+});
